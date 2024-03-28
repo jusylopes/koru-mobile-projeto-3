4c0e2262b0d6e21b1a7474d7d0d44114cdc016c0
Núbia Barcellar <nubiabarcellar@gmail.com>
2024-03-28 14:43:09 -0300

    feat: teste de saida da classe produto

--- main.dart
+++ main.dart
@@ -1,72 +1,30 @@
-// Importações dos arquivos que serão utilizados
-import 'classes/pessoa.dart';
 import 'classes/produto.dart';
 import 'utils.dart';
 
-/**
- * RECOMENDAÇÕES DA TUTORA
- * 
- * Arquivo de testes e execução do sistema criado.
- * Os testes para o sistema se darão todos através de prints no console.
- * 
- * Abaixo você encontrará o método main, que é o método principal que será executado.
- * 
- * Para executar esse arquivo, digite o seguinte comando no terminal: dart run main.dart
- * Outra alternativa é instalar a extensão Dart no seu VS Code.
- * Isso fará aparecer um botão tipo "play" no canto direito superior. Basta clicar para executar o projeto.
- * 
- * Dentro do método main, vocês encontrarão indicações para criar objetos das classes criadas e executar os métodos criados.
- * Recomenda-se que essas declarações e execuções sejam feitas a medida que vocês forem criando as classes e métodos pedidos,
- * pois isso servirá para testar se o que você fez está funcionando.
- * 
- * Não se prendam apenas aos comentários e recomendações. Façam todos os testes que quiserem fazer.
- * 
- * Existe um método "pularLinha" que foi criado dentro do arquivo utils.
- * Caso queiram, utilizem-no para pular linha entre um bloco de testes e outro,
- * apenas por questões de organização e melhor visualização das saídas.
- * Para executá-lo, basta chamá-lo em qualquer ponto do método main, dessa maneira:
- * pularLinha();
- */
+void main() {
+  Produto produto1 = Produto(
+    nome: 'Gel de Limpeza Facial Antioxidante Botik Vitamina C 200g',
+    valor: 69.90,
+    qtdEmEstoque: 2,
+    );
+    produto1.realizarVenda();
+    print("A receita gerada com a venda do produto é de ${produto1.verReceitaGerada()} reais.");
+    pularLinha();
+    produto1.realizarVenda();
+    print("A receita gerada com a venda do produto é de ${produto1.verReceitaGerada()} reais.");
+    pularLinha();
+    produto1.realizarVenda();
+    print("A receita gerada com a venda do produto é de ${produto1.verReceitaGerada()} reais.");
+    pularLinha();
 
-main() {
-  /* Declaração de objetos Produto */
-
-  // Declare aqui alguns objetos do tipo Produto...
-    // Exemplo: Produto produtoA = Produto("Colônia Floratta Flores Secretas 75ml", 104.90, 3);
-
-
-  /* Testes da classe Produto */
-
-  // Teste o método realizarVenda algumas vezes...
-    // Exemplo: produtoA.realizarVenda();
-    // Saída esperada 1: Compra de um produto Colônia Floratta Flores Secretas 75ml realizada com sucesso!
-    // Saída esperada 2: No momento não possuímos o produto Colônia Floratta Flores Secretas 75ml em estoque.
-
-  // Teste o método verReceitaGerada algumas vezes...
-    // Exemplo: print(produtoA.verReceitaGerada());
-
-  // pularLinha();
-
-  /* ------------------------------------------ */
-
-  /* Declaração de objetos Pessoa */
-
-  // Declare aqui alguns objetos do tipo Pessoa...
-    // Exemplo: Pessoa pessoaA = Pessoa('Maria', '12345678900', 1994);
-
-
-  /* Testes da classe Pessoa */
-
-  // Teste o método falar algumas vezes...
-    // Exemplo: pessoaA.falar('Oi, tudo bem?');
-    // Saída esperada: Maria diz: Oi, tudo bem?
-
-  // Teste o método maioridade algumas vezes...
-    // Exemplo: pessoaA.maioridade();
-    // Saída esperada 1: Maria tem 30 anos, portanto é maior de idade.
-    // Saída esperada 2: João tem 14 anos, portanto é menor de idade.
-
-  // pularLinha();
-
-  /* ------------------------------------------ */
+    Produto produto2 = Produto(
+      nome: 'Privée Fresh Season Eau De Parfum 75ml',
+      valor: 399.00,
+      qtdEmEstoque: 1,
+    );
+    produto2.realizarVenda();
+    print("A receita gerada com a venda do produto é de ${produto2.verReceitaGerada()} reais.");
+    pularLinha();
+    produto2.realizarVenda();
+    print("A receita gerada com a venda do produto é de ${produto2.verReceitaGerada()} reais.");
 }
