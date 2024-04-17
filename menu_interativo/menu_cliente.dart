278e5fb861cf971c7aeefe19d97e9d9d4ace73f6
Jusy Lopes <jusylopes@hotmail.com>
2024-04-17 15:48:14 -0300

    feat: regex data de nascimento e cpf (#54)
    * feat: add dados mockados
    
    * feat: add enum usario
    
    * add: utils menu
    
    * feat: menu revendedor
    
    * feat: menu cliente
    
    * feat: menu principal
    
    * feat: add menu principal
    
    * feat: criar titulo de menu
    
    * feat: get genero revendedor
    
    * feat: regex data de nascimento e cpf
    
    * refactor: menu revendedor

--- menu_interativo/menu_cliente.dart
+++ menu_interativo/menu_cliente.dart
@@ -3,8 +3,6 @@ import '../classes/cliente.dart';
 import '../classes/produto.dart';
 import '../classes/revendedor.dart';
 import '../utils/enums.dart';
-import '../utils/mock_produto.dart';
-import '../utils/mock_revendedor.dart';
 import '../utils/utils.dart';
 import 'utils_menu.dart';
 
@@ -16,7 +14,8 @@ class MenuCliente {
 
     while (!sair) {
       pularLinha();
-      imprimirMensagemComMoldura(' CLIENTE    |     TEAM FIVE   ');
+      imprimirMensagemComMoldura(
+          ' CLIENTE |  ${_criaTituloDoMenuCliente(cliente)} ');
       print('|--------------------------------|');
       print('| 1 - Adicionar Cliente          |');
       print('| 2 - Adicionar Dinheiro         |');
@@ -31,36 +30,27 @@ class MenuCliente {
 
       switch (opcao) {
         case '1':
-          cliente = inserirDadosCliente();
+          cliente = _inserirDadosCliente();
           break;
         case '2':
-          if (cliente != null) {
-            print('Digite o valor a ser adicionado:');
-            double valor = double.parse(stdin.readLineSync()!);
-
-            cliente!.adicionarDinheiro(valor);
-          } else {
-            imprimirUsuarioNaoAdicionado(Usuario.cliente);
-          }
+          cliente != null
+              ? _adicionarDinheiro(cliente!)
+              : imprimirUsuarioNaoAdicionado(Usuario.cliente);
           break;
         case '3':
-          if (cliente != null) {
-            _comprarProduto(cliente!);
-          } else {
-            imprimirUsuarioNaoAdicionado(Usuario.cliente);
-          }
+          cliente != null
+              ? _comprarProduto(cliente!)
+              : imprimirUsuarioNaoAdicionado(Usuario.cliente);
           break;
         case '4':
-          if (cliente != null) {
-            cliente!.verProdutosComprados();
-          } else {
-            imprimirUsuarioNaoAdicionado(Usuario.cliente);
-          }
+          cliente != null
+              ? cliente!.verResumo()
+              : imprimirUsuarioNaoAdicionado(Usuario.cliente);
           break;
         case '5':
-          if (cliente != null) {
-            cliente!.verSaldoAtual();
-          } else {}
+          cliente != null
+              ? cliente!.verSaldoAtual()
+              : imprimirUsuarioNaoAdicionado(Usuario.cliente);
           break;
         case '6':
           sair = true;
@@ -72,24 +62,38 @@ class MenuCliente {
     }
   }
 
+  String _criaTituloDoMenuCliente(Cliente? cliente) {
+    return cliente != null
+        ? '${cliente.nome} - saldo \$ ${cliente.dinheiro}'
+        : 'TEAM FIVE';
+  }
+
+  Cliente _inserirDadosCliente() {
+    print('Digite seu nome:');
+    String nome = stdin.readLineSync()!;
+    print('Digite seu CPF (apenas números):');
+    String cpf = validarCpf(stdin.readLineSync()!);
+    print('Digite sua data de nascimento (dessa forma: AAAA-MM-DD):');
+    DateTime dataNascimento =
+        DateTime.parse(validarDataDeNascimento(stdin.readLineSync()!));
+    Genero genero = selecionarGeneroNoMenu();
+
+    Cliente cliente = Cliente(
+        nome: nome, cpf: cpf, dataDeNascimento: dataNascimento, genero: genero);
+
+    return cliente;
+  }
+
+  void _adicionarDinheiro(Cliente cliente) {
+    print('Digite o valor a ser adicionado:');
+    double valor = double.parse(stdin.readLineSync()!);
+
+    cliente.adicionarDinheiro(valor);
+  }
+
   void _comprarProduto(Cliente cliente) {
-    List<Produto> produtos = MockProduto.mockProduto();
-    List<Revendedor> revendedor = MockRevendedor.mockRevendedor();
-
-    print('Escolha um produto:');
-    for (int i = 0; i < produtos.length; i++) {
-      print('${i + 1} - ${produtos[i].nome}');
-    }
-    int produtoIndex = int.parse(stdin.readLineSync()!) - 1;
-
-    print('Escolha um revendedor:');
-    for (int i = 0; i < revendedor.length; i++) {
-      print('${i + 1} - ${revendedor[i].nome}');
-    }
-    int revendedorIndex = int.parse(stdin.readLineSync()!) - 1;
-
-    Produto produtoSelecionado = produtos[produtoIndex];
-    Revendedor revendedorSelecionado = revendedor[revendedorIndex];
+    Produto produtoSelecionado = selecionarProdutoNoMenu();
+    Revendedor revendedorSelecionado = selecionarRevendedorNoMenu();
 
     cliente.comprarProduto(produtoSelecionado, revendedorSelecionado);
   }
